b70c3b921b1a7296fe932e9accfa07765880507c
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-22 14:20:56 -0500

    success_msg with flash

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { extname } = require('path');
 const { mainModule } = require('process');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash');
 
 // Initializations
 const app = express();
@@ -30,8 +31,14 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(flash());
 
 // Global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 // Routes
 app.use(require('./routes/index'));
