f6fe4dcc255ab1188aa3df35396f81de055ca653
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-23 22:16:35 -0500

    signin

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ const { mainModule } = require('process');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
 
 // Initializations
 const app = express();
 require('./database');
+require('./config/passport');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -31,12 +33,15 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 
 // Global variables
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
 });
 
