9a67a6a4215eea431bbd9ec78985458c939e7f5c
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-24 21:11:50 -0500

    select stable db

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://186.178.18.70/notes-app', {
-//mongoose.connect('mongodb+srv://pjlZPARR88TQVbdM:pjlZPARR88TQVbdM@cluster0.x8mia.mongodb.net/test', {
-mongoose.connect('mongodb+srv://pjlZPARR88TQVbdM:pjlZPARR88TQVbdM@cluster0.x8mia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://pjlZPARR88TQVbdM:pjlZPARR88TQVbdM@cluster0.x8mia.mongodb.net/test', {
+//mongoose.connect('mongodb+srv://pjlZPARR88TQVbdM:pjlZPARR88TQVbdM@cluster0.x8mia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
