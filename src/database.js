d6e61e88c478ffa813db8d4b986d5cdd07c8f6c8
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-24 23:34:58 -0500

    make it better

--- src/database.js
+++ src/database.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://186.178.18.70/notes-app', {
-mongoose.connect('mongodb+srv://pjlZPARR88TQVbdM:pjlZPARR88TQVbdM@cluster0.x8mia.mongodb.net/test', {
-//mongoose.connect('mongodb+srv://pjlZPARR88TQVbdM:pjlZPARR88TQVbdM@cluster0.x8mia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://andylenincp:andylenincp@cluster0.x8mia.mongodb.net/notes-app', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
