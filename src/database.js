f2141cf431b3a51c30f60fc1d0ef057f9b9c25ff
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-24 20:54:22 -0500

    connections

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//mongoose.connect('mongodb://186.178.18.70/notes-app', {
 mongoose.connect('mongodb+srv://pjlZPARR88TQVbdM:pjlZPARR88TQVbdM@cluster0.x8mia.mongodb.net/test', {
     useCreateIndex: true,
     useNewUrlParser: true,
