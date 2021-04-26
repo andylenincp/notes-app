a3fe5773809a1517391cbbca24448643497018c1
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-26 12:15:38 -0500

    update database

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://186.178.18.70/notes-app', {
+//mongoose.connect('mongodb://localhost/notes-app', {
 mongoose.connect('mongodb+srv://andylenincp:andylenincp@cluster0.x8mia.mongodb.net/notes-app', {
     useCreateIndex: true,
     useNewUrlParser: true,
